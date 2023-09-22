469729783a78f504058693f55dea316579f6f23c
Javi <a21javierbq@iessanclemente.net>
2023-09-22 14:25:13 +0200

    ChronometerBegin

---
+++ UD1_3/app/src/main/java/com/example/ud1_3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ud1_3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
