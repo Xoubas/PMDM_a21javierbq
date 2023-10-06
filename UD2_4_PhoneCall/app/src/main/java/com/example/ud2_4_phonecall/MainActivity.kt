257ca09c6887fbc0f8026a4e4451f3f3284a3404
Javi <a21javierbq@iessanclemente.net>
2023-10-06 14:27:08 +0200

    CroppedImages

--- UD2_4_PhoneCall/app/src/main/java/com/example/ud2_4_phonecall/MainActivity.kt
+++ UD2_4_PhoneCall/app/src/main/java/com/example/ud2_4_phonecall/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.ud2_4_phonecall
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val llamar =findViewById<Button>(R.id.but)
+        val textLamar = findViewById<TextView>(R.id.showCall)
+
+        llamar.setOnClickListener{
+            textLamar.text="CALLING"
+        }
     }
 }
---
+++ UD2_5_Images/app/src/main/java/com/example/ud2_5_images/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ud2_5_images
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
